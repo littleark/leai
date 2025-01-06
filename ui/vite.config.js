2bc0a37c77f7c69da8ec134b90fdd3e70ce5d843
carlo <littleark@gmail.com>
2025-01-06 14:36:27 +0100

    set leai/ as base url

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -4,4 +4,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    outDir: 'dist', // Ensure the output directory is set to 'dist'
+    assetsDir: 'assets', // Ensure the assets directory is set to 'assets'
+  },
+  base: '/leai/', // Set the base URL to the subdirectory where your project is served
 })
