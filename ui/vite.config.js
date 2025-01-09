425a7e797a84dcf4a1751f372e1e5891a52af5d2
carlo <littleark@gmail.com>
2025-01-09 12:13:28 +0100

    copying 404.html in the dist folder

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import copy from 'rollup-plugin-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    copy({
+      targets: [
+        { src: 'src/404.html', dest: 'dist' }, // Copy 404.html to dist
+      ],
+      hook: 'writeBundle', // Ensure it runs at the end of the build process
+    })
+  ],
   build: {
     outDir: 'dist', // Ensure the output directory is set to 'dist'
     assetsDir: 'assets', // Ensure the assets directory is set to 'assets'
